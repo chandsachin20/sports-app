bd15c475d7e1e2eff4be5e6b9dd19390571b50b4
chandsachin20 <sachinchand20@gmail.com>
2020-06-19 22:38:52 +0530

    added socket.io

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -1,5 +1,7 @@
 const Event = require("../models/Event");
 const User = require("../models/User");
+const jwt = require('../config/verifyToken')
+
 
 module.exports = {
   createEvent(req, res) {
--- src/server.js
+++ src/server.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 const routes = require('./routes');
 const path = require("path")
 const cors = require("cors");
+const http = require('http');
+const socketio = require('socket.io')
 const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log('user is connected', socket.id);
+})
 
 const userController = require("./controllers/UserController");
 
@@ -24,7 +33,7 @@ try {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  console.log("mongo db connected succesflly");
+  console.log("mongo db connected succesfully");
     } catch (error) {
   console.log(error);
 }
@@ -33,6 +42,6 @@ try {
 app.use('/files', express.static(path.resolve(__dirname,"..","..","files")));
 app.use(routes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
